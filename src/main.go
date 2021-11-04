9f77d639e5c5c10b3b8ce4113cbb91591e488023
gllb <gleblanc@protonmail.com>
2021-11-04 15:56:17 +0100

    Cleanup main.go

--- src/main.go
+++ src/main.go
@@ -17,19 +17,26 @@ import (
 var db *sql.DB
 const tablename = "birthday"
 
+const count_table_query = `SELECT COUNT(*)
+FROM pg_catalog.pg_tables
+WHERE tablename = $1;`
+const create_table_query = `CREATE TABLE birthday (
+name varchar(30),
+birthday date
+);`
+const get_birthday_query = `SELECT birthday
+FROM birthday
+WHERE name = $1`
+const insert_birthday_query = `INSERT INTO birthday
+VALUES ($1, $2)`
+
+
 type birthday struct {
 	name        string
 	dateOfBirth time.Time `json:dateOfBirth`
 }
 
 const shortForm = "2006-01-02"
-
-// func newBirthday(date date, name string) (*birthday, error) {
-// 	return &birthday{
-// 		dateOfBirth: t,
-// 		name: name,
-// 	}, nil
-// }
 
 func newBirthday(date string, name string) (*birthday, error) {
 	t, _ := time.Parse(shortForm, date)
@@ -42,24 +49,8 @@ func newBirthday(date string, name string) (*birthday, error) {
 	}, nil
 }
 
-// func (birthday *Birthday) MarshalJSON() ([]byte, error) {
-// 	log.Println("MarshalJSON")
-// 	type Alias Birthday
-// 	return json.Marshal(&struct {
-// 		*Alias
-// 		dateOfBirth string `json:"dateOfBirth"`
-// 	}{
-// 		Alias: (*Alias)(birthday),
-// 		dateOfBirth: birthday.dateOfBirth.Format(shortForm),
-// 	})
-// }
-
-func formatBirthday(birthday birthday) string {
-	return birthday.dateOfBirth.Format(shortForm)
-}
-
-
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	initDatabaseConnection()
 	defer db.Close()
 	initDatabaseTable()
@@ -74,12 +65,15 @@ func main() {
 	log.Fatal(err)
 }
 
+/*
+   initDatabaseConnection read environment variable and connect to the configured database
+*/
 func initDatabaseConnection() {
 	var err error
 
 	host := os.Getenv("DBHOST")
 	if host == "" {
-		panic("environment variable DBHOST is empty")
+		log.Fatal("environment variable DBHOST is empty")
 	}
 
 	port := os.Getenv("DBPORT")
@@ -89,17 +83,17 @@ func initDatabaseConnection() {
 
 	dbname := os.Getenv("DBNAME")
 	if dbname == "" {
-		panic("environment variable DBNAME is empty")
+		log.Fatal("environment variable DBNAME is empty")
 	}
 
 	user := os.Getenv("DBUSER")
 	if user == "" {
-		panic("environment variable DBUSER is empty")
+		log.Fatal("environment variable DBUSER is empty")
 	}
 
 	password := os.Getenv("DBPASSWORD")
 	if password == "" {
-		panic("environment variable DBPASSWORD is empty")
+		log.Fatal("environment variable DBPASSWORD is empty")
 	}
 
 	sslmode := os.Getenv("DBSSLMODE")
@@ -111,28 +105,26 @@ func initDatabaseConnection() {
 
 	db, err = sql.Open("postgres", pgsqlInfo)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	err = db.Ping()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	log.Println("Successfully connected to the database!")
 }
-
+/*
+   initDatabaseTable create the necessary table if it does not exist
+ */
 func initDatabaseTable() {
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	var count int
-	const count_table_query = `SELECT COUNT(*)
-FROM pg_catalog.pg_tables
-WHERE tablename = $1;`
 
 	rows, err := db.Query(count_table_query, tablename)
 	defer rows.Close()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	rows.Next()
@@ -141,97 +133,135 @@ WHERE tablename = $1;`
 	}
 	if count == 0 {
 		log.Printf("Create %s table", tablename)
-		const create_table_query = `CREATE TABLE birthday (
-name varchar(30),
-birthday date
-);`
+
 		if _, err := db.Query(create_table_query); err != nil {
 			log.Fatal(err)
 		}
-	// 	columns, _ := rows.Columns()
-	// 	log.Println(strings.Join(columns[:], ", "))
-	// 	rows.Next()
-	// 	if err := rows.Scan(&count); err != nil {
-	// 		log.Fatal(err)
-	// 	}
 	} else {
 		log.Printf("Found existing %s database", tablename)
 	}
 }
-
+/*
+   birthdaySave is an http.Handler:
+   Read username from url and birthday from json body
+   and Write a birthday related to a username in the database
+ */
 func birthdaySave(w http.ResponseWriter, r *http.Request) {
 	var data map[string]string
 	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
-		log.Println("ERROR:", err)
 		w.WriteHeader(http.StatusBadRequest)
+		log.Println("ERROR", err)
 		return
 	}
 	log.Println(data["dateOfBirth"])
 	name := strings.Split(r.URL.Path, "/")[2]
 	birthday, err := newBirthday(data["dateOfBirth"], name)
 	if err != nil {
-		log.Println("ERROR:", err)
 		w.WriteHeader(http.StatusBadRequest)
+		log.Println("ERROR", err)
 		return
 	}
 	log.Println("Insert new birthday: ", birthday.dateOfBirth, birthday.name)
-	const insert_birthday_query = `INSERT INTO birthday
-VALUES ($1, $2)`
+
 	if _, err := db.Query(insert_birthday_query, birthday.name, birthday.dateOfBirth); err != nil {
-		log.Println("ERROR", err)
 		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("ERROR", err)
 		return
 	}
 	w.WriteHeader(http.StatusNoContent)
 }
 
+/*
+   birthdayGet is an http.Handler:
+   Fetch a birthday in database from a username in the url request
+   and write a birthday information message in the response
+*/
 func birthdayGet(w http.ResponseWriter, r *http.Request) {
-	const get_birthday_query = `SELECT birthday
-FROM birthday
-WHERE name = $1`
 	name := strings.Split(r.URL.Path, "/")[2]
 	var dateOfBirth time.Time
 	log.Println("Read birthday of", name)
 	rows, err := db.Query(get_birthday_query, name)
 	if err != nil {
-		log.Println("ERROR", err)
 		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("ERROR", err)
 		return
-
 	}
 	rows.Next()
 	if err := rows.Scan(&dateOfBirth); err != nil {
-		log.Println("ERROR", err)
 		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("ERROR", err)
 		return
 	}
 	var birthday birthday
 	birthday.dateOfBirth = dateOfBirth
 	birthday.name = name
 	if err != nil {
-		log.Println("ERROR", err)
 		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("ERROR", err)
+		return
+	}
+	year, month, day := time.Now().Date()
+	today, err := time.Parse(shortForm, fmt.Sprintf("%d-%02d-%02d", year, month, day))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("ERROR", err)
 		return
 	}
 	age := time.Now().Year() - birthday.dateOfBirth.Year()
 	birthday_current_year := birthday.dateOfBirth.AddDate(age, 0, 0)
-	remainingDays := int(birthday_current_year.Sub(time.Now()).Hours()) / 24
 
 	var message string
-	// TODO: Handle the case of a birthday already passed this year (remainingDays < 0)
-	if remainingDays < 1 && birthday.dateOfBirth.Day() == time.Now().Day() {
+
+	if today.Equal(birthday_current_year) {
+		// Birthday is today
 		message = fmt.Sprintf("Hello, %s! Happy birthday!", birthday.name)
 	} else {
+		var remainingDays int
+		if today.After(birthday_current_year) {
+			// Birthday is next year
+			birthday_next_year := birthday_current_year.AddDate(1, 0, 0)
+			remainingDays = int(birthday_next_year.Sub(time.Now()).Hours()) / 24		
+		} else {
+			// Birthday is this year
+			remainingDays = int(birthday_current_year.Sub(time.Now()).Hours()) / 24
+		}
 		message = fmt.Sprintf("Hello, %s! Your birthday is in %d day(s)", birthday.name, remainingDays)
 	}
+
 	data := map[string]string{"message": message}
 	jsondata, err := json.Marshal(data)
 	if err != nil {
-		log.Println("ERROR", err)
 		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("ERROR", err)
 		return
 	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Write(jsondata)
+}
+
+func computeRemainingDays(date string) (int, error) {
+	birthday_time, err := time.Parse(shortForm, date)
+	if err != nil {
+		return -1, err
+	}
+	year, month, day := time.Now().Date()
+	today, err := time.Parse(shortForm, fmt.Sprintf("%d-%02d-%02d", year, month, day))
+	if err != nil {
+		return -1, err
+	}
+	age := time.Now().Year() - birthday_time.Year()
+	birthday_current_year := birthday_time.AddDate(age, 0, 0)
+
+	var remainingDays int
+	if today.After(birthday_current_year) {
+		// Birthday is next year
+		birthday_next_year := birthday_current_year.AddDate(1, 0, 0)
+		remainingDays = int(birthday_next_year.Sub(time.Now()).Hours()) / 24		
+	} else {
+		// Birthday is this year
+		remainingDays = int(birthday_current_year.Sub(time.Now()).Hours()) / 24
+	}
+
+	return remainingDays, nil
 }
