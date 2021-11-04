6d960d8e37cb71a1bd824dfe5b32ad18843ab4a3
gllb <gleblanc@protonmail.com>
2021-11-04 15:54:00 +0100

    Improve test

--- src/helloworld_test.go
+++ src/helloworld_test.go
@@ -7,8 +7,38 @@ import (
 	"encoding/json"
 	"bytes"
 	"fmt"
+	"time"
 	// "log"
 )
+
+func TestSaveBirthdayInFuture(t *testing.T) {
+	initDatabaseConnection()
+	defer db.Close()
+	initDatabaseTable()
+
+	name := "toto"
+	birthday := map[string]string{"dateOfBirth": "2050-10-10"}
+	jsondata, err := json.Marshal(birthday)
+	if err != nil {
+		panic(err)
+	}
+	t.Run("Save birthday in the future", func(t *testing.T) {
+		url := fmt.Sprintf("/hello/%s", name)
+		request, err := http.NewRequest(http.MethodPut, url, bytes.NewBuffer(jsondata))
+		if err != nil {
+			panic(err)
+		}
+		response := httptest.NewRecorder()
+		birthdaySave(response, request)
+
+		got := response.Code
+		want := http.StatusBadRequest
+		if got != want {
+			t.Errorf("got HTTP status %d, want %d", got, want)
+		}
+
+	})
+}
 
 func TestGetBirthday(t *testing.T) {
 	initDatabaseConnection()
@@ -16,10 +46,31 @@ func TestGetBirthday(t *testing.T) {
 	initDatabaseTable()
 
 	name := "guillaume"
-	birthday := map[string]string{"dateOfBirth": "1994-12-20" }
+	birthday := map[string]string{"dateOfBirth": "1994-12-20"}
 	jsondata, err := json.Marshal(birthday)
 	if err != nil {
 		panic(err)
+	}
+
+	year, month, day := time.Now().Date()
+	today, err := time.Parse(shortForm, fmt.Sprintf("%d-%02d-%02d", year, month, day))
+	if err != nil {
+		panic(err)
+	}
+	birthday_time, err := time.Parse(shortForm, birthday["dateOfBirth"])
+	if err != nil {
+		panic(err)
+	}
+	age := time.Now().Year() - birthday_time.Year()
+	birthday_current_year := birthday_time.AddDate(age, 0, 0)
+	var remainingDays int
+	if today.After(birthday_current_year) {
+		// Birthday is next year
+		birthday_next_year := birthday_current_year.AddDate(1, 0, 0)
+		remainingDays = int(birthday_next_year.Sub(time.Now()).Hours()) / 24		
+	} else {
+		// Birthday is this year
+		remainingDays = int(birthday_current_year.Sub(time.Now()).Hours()) / 24
 	}
 
 	t.Run("Save birthday date", func(t *testing.T) {
@@ -32,7 +83,7 @@ func TestGetBirthday(t *testing.T) {
 		birthdaySave(response, request)
 
 		got := response.Code
-		want := 204
+		want := http.StatusNoContent
 		if got != want {
 			t.Errorf("got HTTP status %d, want %d", got, want)
 		}
@@ -51,7 +102,6 @@ func TestGetBirthday(t *testing.T) {
 		}
 
 
-		remainingDays := 47
 		got := response.Body.String()
 		want := fmt.Sprintf(`{"message":"Hello, %s! Your birthday is in %d day(s)"}`, name, remainingDays)
 		if got != want {
